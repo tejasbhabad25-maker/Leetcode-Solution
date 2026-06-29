aff6b3308864c3b1979591c2d43aac155ec042f5
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-06-29 17:21:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.68%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -4,15 +4,9 @@ public:
         if(n==0 || n<0){
             return false;
         }
-        while(n>0){
-            if(n==1){
-                return true;
-            }
-            if(n%2!=0){
-                return false;
-            }
-            n=n/2;
+        if((n & n-1)==0){
+            return true;
         }
-        return true;
+        return false;
     }
 };
