e11c20840a2be0b2f61334542466588f4fad13ce
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-07-02 16:31:11 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.74%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     int searchInsert(vector<int>& arr, int tar) {
-        
-        int ans=lower_bound(arr.begin(),arr.end(),tar)-arr.begin();
+
+        int n=arr.size();
+        int ans=n;
+
+        int st=0 , end=n-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+
+            if(arr[mid]>=tar){
+                end=mid-1;
+                ans=mid;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        return ans;
         
         return ans;
     }
