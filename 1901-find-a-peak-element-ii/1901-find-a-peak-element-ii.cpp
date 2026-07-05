d6529849acb7ad9bad756ae6d64f4e57e1b7ed1a
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-07-05 14:54:17 +0530

    Time: 0 ms (100%), Space: 50.3 MB (41.89%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -7,7 +7,6 @@ public:
         int st=0 , end=m-1;
         
         int row=-1;
-        vector<int>ans;
 
         while(st<=end){
             // calculate max_el for each col
@@ -20,37 +19,20 @@ public:
                     row=i;
                 } 
             }
-            if(mid==0){
-                if(arr[row][0]>arr[row][1]){
-                    ans.push_back(row);
-                    ans.push_back(mid);
-                    return ans;
-                }
-                st=mid+1;
+            
+            int left=mid-1>=0?arr[row][mid-1]:-1;
+            int right=mid+1<m?arr[row][mid+1]:-1;
+
+            if(arr[row][mid]>left && arr[row][mid]>right){
+                return {row,mid};
             }
-            else if(mid==m-1){
-                if(arr[row][m-1]>arr[row][m-2]){
-                    ans.push_back(row);
-                    ans.push_back(mid);
-                    return ans;
-                }
+            else if(arr[row][mid]<left){
                 end=mid-1;
             }
             else{
-                if(max_el>arr[row][mid-1] && max_el>arr[row][mid+1]){
-                ans.push_back(row);
-                ans.push_back(mid);
-                return ans;
-                }
-                else if(arr[row][mid-1] > max_el){
-                    end=mid-1;
-                }
-                else{
-                    st=mid+1;
-                }
+                st=mid+1;
             }
-
         }
-        return ans;
+        return {-1,-1};
     }
 };
