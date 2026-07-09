521852d4d6975dfbea6a955c8b2a9066a7215591
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-07-09 23:12:10 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.83%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
-        int n = arr1.size(), m = arr2.size();
-        int k = m + n;
-        double res;
+        if (arr1.size() > arr2.size())
+            return findMedianSortedArrays(arr2, arr1);
 
-        vector<int> ans;
+        int n1 = arr1.size();
+        int n2 = arr2.size();
 
-        for (int i = 0; i < n; i++) {
-            ans.push_back(arr1[i]);
+        int st = 0, end = n1;
+
+        while(st<=end){
+
+            int mid=st+(end-st)/2;
+
+            int partitionX=mid;
+            int partitionY=(n1+n2+1)/2-partitionX;
+
+            int l1=partitionX==0?INT_MIN:arr1[partitionX-1];
+            int l2=partitionY==0?INT_MIN:arr2[partitionY-1];
+
+            int r1=partitionX==n1?INT_MAX:arr1[partitionX];
+            int r2=partitionY==n2?INT_MAX:arr2[partitionY];
+
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==0){
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                }
+                else{
+                    return max(l1,l2);
+                }
+            }
+            else if(l1>r2){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
         }
-
-        for (int i = 0; i < m; i++) {
-            ans.push_back(arr2[i]);
-        }
-        sort(ans.begin(), ans.end());
-
-        int st = 0, end = k - 1;
-        int mid = st + (end - st) / 2;
-
-        if (k % 2 != 0) {
-            res = ans[mid];
-        } else {
-            res = ans[mid] + ans[mid + 1];
-            res = res / 2;
-        }
-        return res;
+        return -1;
     }
 };
