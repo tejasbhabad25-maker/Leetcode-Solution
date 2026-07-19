4f1bf0dd98342eed9b16571c752cbdae25fa7c02
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-07-19 18:36:04 +0530

    Time: 14 ms (92.26%), Space: 24.1 MB (72.01%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& arr) {
-        
-        int n=arr.size();
+        int n;
 
-        // max el
-        int max_el=INT_MIN;
-        int max_idx=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>max_el){
-                max_el=arr[i];
-                max_idx=i;
-            }
-        }
-        // second max
-        int Smax_el=INT_MIN;
-        for(int i=0;i<n;i++){
-            if(arr[i]>Smax_el && max_idx!=i){
-                Smax_el=arr[i];
-            }
-        }
+        int max_el=INT_MIN , Smax_el=INT_MIN;
+        int min_el=INT_MAX , Smin_el=INT_MAX;
 
-        // min el
-        int min_el=INT_MAX;
-        int min_idx=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]<min_el){
-                min_el=arr[i];
-                min_idx=i;
+        for(int v:arr){
+
+            if(v>max_el){
+                Smax_el=max_el;
+                max_el=v;
             }
-        }
-        // sec min el
-        int Smin_el=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(arr[i]<Smin_el && min_idx!=i){
-                Smin_el=arr[i];
+            else if(v>Smax_el){
+                Smax_el=v;
+            }
+
+            if(v<min_el){
+                Smin_el=min_el;
+                min_el=v;
+            }
+            else if(v<Smin_el){
+                Smin_el=v;
             }
         }
 
-        long long ans = abs(((long long)max_el*Smax_el)-((long long)min_el*Smin_el));
+        long long ans=abs((long long)max_el*Smax_el-(long long)min_el*Smin_el);
         return ans;
     }
 };
