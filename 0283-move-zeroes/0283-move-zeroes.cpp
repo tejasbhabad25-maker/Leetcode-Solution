1f55d66a12e862b7eb29188c705a74df5541d04b
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-07-19 20:03:20 +0530

    Time: 0 ms (100%), Space: 23.7 MB (98.94%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n=arr.size();
+
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                swap(arr[i],arr[j]);
+                j++;
+            }
+        }
+    }
+};
