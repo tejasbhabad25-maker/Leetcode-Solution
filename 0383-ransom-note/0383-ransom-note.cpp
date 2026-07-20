1c8ef4ad99a877c963f5a3a4f72cdb805535cdf0
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-07-20 19:10:29 +0530

    Time: 2 ms (58.62%), Space: 11.7 MB (75.02%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int n1=ransomNote.size();
-        int n2=magazine.size();
-
-        if(n1>n2){
-            return false;
+        
+        vector<int>freq(26,0);
+        
+        for(char v:magazine){
+            freq[v-'a']++;
         }
-        vector<int>rfreq(26,0);
-        vector<int>mfreq(26,0);
 
         for(char v:ransomNote){
-            rfreq[v-'a']++;
-        }
-        for(char v:magazine){
-            mfreq[v-'a']++;
-        }
-
-        for(int i=0;i<26;i++){
-            if(rfreq[i]>mfreq[i]){
+            if(freq[v-'a']==0){
                 return false;
             }
+            freq[v-'a']--;
         }
         return true;
     }
