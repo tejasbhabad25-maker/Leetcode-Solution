4dc7fce63aabded21291bd7b2013aec6f31d8a30
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-08-06 23:43:21 +0530

    Time: 0 ms (100%), Space: 9 MB (38.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        int n=s.size();
+        if(n%2!=0){
+            return false;
+        }
+
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='(' || ch=='[' || ch=='{' ){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(st.top()=='(' && ch==')'){
+                    st.pop();
+                }
+                else if(st.top()=='[' && ch==']'){
+                    st.pop();
+                }
+                else if(st.top()=='{' && ch=='}'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
