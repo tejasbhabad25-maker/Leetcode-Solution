5c9115ce00516a82e881a5a77d178068df6f6715
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-08-08 21:36:36 +0530

    Time: 3 ms (44.57%), Space: 16.7 MB (5.36%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-
-        vector<vector<int>>mat;
-
-        for(int x:arr){
-            int ct=0;
-            for(int i=0;i<32;i++){
-                if(x & (1<<i)){
-                    ct++;
-                }
-            }
-            mat.push_back({ct,x});
-        }
-        sort(mat.begin(),mat.end());
         
-        vector<int>ans;
-        for(int i=0;i<mat.size();i++){
+        vector<vector<int>> mat;
+
+        for (int x : arr) {
+            int original = x;
+            int ct = 0;
+
+            while (x) {
+                // here we are changing value of x thus use other var to store x i.e original
+                x = x & (x - 1);
+                ct++;
+            }
+
+            mat.push_back({ct, original});
+        }
+
+        sort(mat.begin(), mat.end());
+
+        vector<int> ans;
+
+        for (int i = 0; i < mat.size(); i++) {
             ans.push_back(mat[i][1]);
         }
-        
+
         return ans;
     }
 };
