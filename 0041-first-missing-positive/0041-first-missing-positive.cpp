adc69f9ed88869811a49c0de940f7ee2e0cc9697
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-08-26 23:41:12 +0530

    Time: 6 ms (33.04%), Space: 54.9 MB (86.15%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
+    int firstMissingPositive(vector<int>& arr) {
+        
+        int n=arr.size();
 
-        unordered_set<int> s(nums.begin(), nums.end());
-        int n = nums.size();
-
-        for (int i = 1; i <= n; i++) {
-            if(s.find(i)==s.end()){
-                return i;
+        // cleaning the array
+        // if we get any number greater size of arr or a negative number
+        //  then mark it out of bound here we are using n+1
+        for(int i=0;i<n;i++){
+            if(arr[i]<=0 || arr[i]>n){
+                arr[i]=n+1;
             }
         }
-        return n + 1;
+
+        // marks the presence
+        for(int i=0;i<n;i++){
+            int num=abs(arr[i]);
+
+            if(num>arr.size()){
+                continue;
+            }
+
+            if(arr[num-1]>0){
+                arr[num-1]=-arr[num-1];
+            }
+        }
+        
+
+        // finding first missing positive
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                return i+1;
+            }
+        }
+
+        // if number not exist in arr then it will bw n+1
+        return n+1;
     }
 };
