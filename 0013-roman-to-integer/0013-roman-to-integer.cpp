8f55488b82be1b5537db3fa233095cf64b52813d
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-08-28 09:01:04 +0530

    Time: 8 ms (41.52%), Space: 12.2 MB (70.49%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        unordered_map<char,int>m;
+        m['I']=1; m['V']=5; m['X']=10; m['L']=50; m['C']=100; m['D']=500; m['M']=1000;
+
+        int n=s.size();
+        int num=m[s[n-1]];
+
+        for(int i=n-2;i>=0;i--){
+            if(m[s[i]]>=m[s[i+1]]){
+                num+=m[s[i]];
+            }
+            else{
+                num-=m[s[i]];
+            }
+        }
+        return num;
+    }
+};
