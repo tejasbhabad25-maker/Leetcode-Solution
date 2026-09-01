dd20df447f09dffe1cac1a44cd854b10137774ab
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-09-01 22:23:50 +0530

    Time: 3 ms (99.79%), Space: 80.6 MB (84.01%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& arr) {
         
-        stack<int>st;
         int ans=0;
         int n=arr.size();
+        vector<int>st;
+        st.reserve(n+1);
 
         for(int i=0;i<n;i++){
             
-            while(!st.empty() && arr[st.top()]>arr[i]){
-                int a=st.top();
-                st.pop();
+            while(!st.empty() && arr[st.back()]>arr[i]){
+                int a=st.back();
+                st.pop_back();
 
                 int nse=i;
-                int pse=st.empty()?-1:st.top();
+                int pse=st.empty()?-1:st.back();
 
                 ans=max(ans,arr[a]*(nse-pse-1));
             }
-            st.push(i);
+            st.push_back(i);
         }
         while(!st.empty()){
             // as there are elements in the stack which means there is no nse
             int nse=n;
-            int a=st.top();
-            st.pop();
+            int a=st.back();
+            st.pop_back();
 
-            int pse=st.empty()?-1:st.top();
+            int pse=st.empty()?-1:st.back();
             ans=max(ans,arr[a]*(nse-pse-1));
         }
         return ans;
