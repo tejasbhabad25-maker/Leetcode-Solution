205a3cc2b80fc807e3ef2cad09e08e4369af8fdb
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-09-01 22:19:57 +0530

    Time: 7 ms (98.46%), Space: 81.4 MB (72.24%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
-    vector<int> nse(vector<int> arr) {
-
-        stack<int> st;
-        vector<int> ans(arr.size(), 0);
-
-        for (int i = arr.size() - 1; i >= 0; i--) {
-
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-            if (st.empty()) {
-                ans[i] = -1;
-            } else {
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> pse(vector<int> arr) {
-
-        stack<int> st;
-        vector<int> ans(arr.size(), 0);
-        // 2,1,5,6,2,3
-
-        for (int i = 0; i < arr.size(); i++) {
-
-            while (!st.empty() && arr[st.top()] > arr[i]) {
-                st.pop();
-            }
-            if (st.empty()) {
-                ans[i] = -1;
-            } else {
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-
     int largestRectangleArea(vector<int>& arr) {
-        int n = arr.size();
-        int ans = INT_MIN;
+        
+        stack<int>st;
+        int ans=0;
+        int n=arr.size();
 
-        vector<int> NSE = nse(arr); //   1 -1 4 4 -1 -1
-        vector<int> PSE = pse(arr); //  -1 -1 1 2  1  4
+        for(int i=0;i<n;i++){
+            
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                int a=st.top();
+                st.pop();
 
-        for (int i = 0; i < n; i++) {
+                int nse=i;
+                int pse=st.empty()?-1:st.top();
 
-            int left = PSE[i] == -1 ? -1 : PSE[i];
-            int right = NSE[i] == -1 ? n : NSE[i];
+                ans=max(ans,arr[a]*(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            // as there are elements in the stack which means there is no nse
+            int nse=n;
+            int a=st.top();
+            st.pop();
 
-            int len = abs(right - left - 1);
-            ans = max(ans, len * arr[i]);
+            int pse=st.empty()?-1:st.top();
+            ans=max(ans,arr[a]*(nse-pse-1));
         }
         return ans;
     }
