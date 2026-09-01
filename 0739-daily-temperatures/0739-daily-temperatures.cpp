e664b891e19d43dfd7d73d615ed097b41ef8686c
Tejas Bhabad <tejasbhabad25@gmail.com>
2026-09-01 09:07:54 +0530

    Time: 16 ms (80.77%), Space: 102.7 MB (91.36%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& arr) {
+        
+        int n=arr.size();
+        vector<int>ans(n,0);
+        stack<int>st;
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty() && arr[st.top()]<=arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans[i]=0;
+            }
+            else{
+                ans[i]=(st.top()-i);
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
